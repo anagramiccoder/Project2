eb93037d63f57389f4a77afaebcbcee6f236e800
anagramiccoder <jdvidad31@gmail.com>
2022-01-16 07:29:38 +0800

    added Threading

--- file_server.c
+++ file_server.c
@@ -3,18 +3,20 @@
 #include <string.h>
 #include<time.h>
 #include <unistd.h>
-typedef struct worker_thread{
+#include<pthread.h>
+typedef struct workerthread{
 	pthread_t id;
-	struct worker_thread *next;
-}work;
+	struct workerthread *next;
+}worker;
 time_t t;
 char command[150];
 FILE *cmdfile;
-void rfile(char *add);
-void wfile(char *cmd);
-void efile(char *add);
-void worker(char *cmd);
+void toread(char *add);
+void towrite(char *cmd);
+void toempty(char *add);
+void towork(char *cmd);
 int main(){
+	worker *head=NULL;
 	while(1){
 		printf(">>");
 		scanf("%[^\n]%*c", command);
@@ -25,33 +27,36 @@ int main(){
 		fclose(cmdfile);
 		char *wcmd=(char *) malloc(150 *sizeof(char));
 		strcpy(wcmd,command);
-		worker(wcmd);
+		worker *temp=malloc(sizeof(worker));
+		temp->next=head;
+		head=temp;
+		pthread_create(&head->id,NULL,(void *) towork,wcmd);
 	}
 	return 0;
 }
 
-void worker(char  *cmd){
+void towork(char  *cmd){
 	int del=rand()%10;
 	if(del==0 || del==1){
-	sleep(0.6);
+	sleep(6);
 	}
 	else{
-	sleep(0.6);
+	sleep(1);
 	}
 	if (cmd[0]=='r' && cmd[1]=='e' && cmd[2]=='a' && cmd[3]=='d'){
-		rfile(&cmd[5]);
+		toread(&cmd[5]);
 	}
 	else if(cmd[0]=='e' && cmd[1]=='m' && cmd[2]=='p' && cmd[3]=='t' && cmd[4]=='y'){
-		efile(&cmd[6]);
+		toempty(&cmd[6]);
 	}
 	else if(cmd[0]=='w' && cmd[1]=='r' && cmd[2]=='i' && cmd[3]=='t' && cmd[4]=='e'){
-		wfile(&cmd[6]);
+		towrite(&cmd[6]);
 	}
 	else{
 		printf("ERROR: no such command\n");
 	}
 }
-void rfile(char *add){
+void toread(char *add){
 	FILE *rf;
 	rf=fopen(add,"r");
 	char *input="read";
@@ -65,9 +70,8 @@ void rfile(char *add){
 	rf=fopen("read.txt","a+");
 	fprintf(rf,"%s %s: %s\n",input,add,data);
 	fclose(rf);
-	free(add);
 }
-void efile(char *add){
+void toempty(char *add){
 	FILE *ef;
 	ef=fopen(add,"r");
 	char *input="empty";
@@ -82,9 +86,11 @@ void efile(char *add){
 	ef=fopen("empty.txt","a+");
 	fprintf(ef,"%s %s: %s\n",input,add,data);
 	fclose(ef);
-	free(add);
+	int delay=rand()%3001;
+	double sec=(7000+delay)/1000;
+	sleep(sec);
 }
-void wfile(char *cmd){
+void towrite(char *cmd){
 	FILE *wf;
 	int count=0;
 	char *p=cmd;
@@ -95,5 +101,12 @@ void wfile(char *cmd){
 	char add[count-1];
 	strcpy(add,cmd);
 	add[count]='\0';
-	printf("%s\n",&cmd[count+1]);
+	p=&cmd[count+1];
+	wf=fopen(add,"a+");
+	while(*p!='\0'){
+		fputc(*p,wf);
+		sleep(0.025);
+		p++;
+	}
+	fclose(wf);
 }
