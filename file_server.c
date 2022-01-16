143254f91a42fdfc7d972a1c5a4ee80b154b7560
anagramiccoder <jdvidad31@gmail.com>
2022-01-16 09:25:22 +0800

    added comments

--- file_server.c
+++ file_server.c
@@ -19,16 +19,16 @@ int main(){
 	worker *head=NULL;
 	while(1){
 		printf(">>");
-		scanf("%[^\n]%*c", command);
+		scanf("%[^\n]%*c", command);//scanf command which gets the input, denoted by \n on the end
 		if(strcmp(command,"exit")==0){break;}
-		time(&t);
-		cmdfile=fopen("command.txt","a+");
+		time(&t);//update time after getting command
+		cmdfile=fopen("command.txt","a+"); //a+ to append if file present, create if not.
 		fprintf(cmdfile,"%s %s",command,ctime(&t));
 		fclose(cmdfile);
 		char *wcmd=(char *) malloc(150 *sizeof(char));
 		strcpy(wcmd,command);
 		worker *temp=malloc(sizeof(worker));
-		temp->next=head;
+		temp->next=head;//earlier threads goes to the tail, meaning the nearer it is in the head, the more recent the thread is
 		head=temp;
 		pthread_create(&head->id,NULL,(void *) towork,wcmd);
 	}
@@ -49,6 +49,7 @@ void towork(char  *cmd){
 	else{
 	sleep(1);
 	}
+	//did not use strcmp, since this is one of the unique ways of how I implement it
 	if (cmd[0]=='r' && cmd[1]=='e' && cmd[2]=='a' && cmd[3]=='d'){
 		toread(&cmd[5]);
 	}
@@ -58,7 +59,7 @@ void towork(char  *cmd){
 	else if(cmd[0]=='w' && cmd[1]=='r' && cmd[2]=='i' && cmd[3]=='t' && cmd[4]=='e'){
 		towrite(&cmd[6]);
 	}
-	else{
+	else{//just incase it is not from the 4 commands, read, write, empty, exit-- note that for exit, it willnot create a new thread
 		printf("ERROR: no such command\n");
 	}
 }
@@ -66,13 +67,13 @@ void toread(char *add){
 	FILE *rf;
 	rf=fopen(add,"r");
 	char *input="read";
-	char data[50]="FILE DNE";
+	char data[50]="FILE DNE";//set default value to FILE DNE,
 	if(rf!=NULL){
 		char tempdata[50];
 		fgets(tempdata,50,rf);
 		fclose(rf);
 		strcpy(data,tempdata);
-		if(strlen(tempdata)>0){
+		if(strlen(tempdata)>0){//check if existing file but empty content
 		int len=strlen(data);
 		data[len-1]='\0';
 		}
@@ -112,18 +113,18 @@ void towrite(char *cmd){
 	FILE *wf;
 	int count=0;
 	char *p=cmd;
-	while(*p!=' '){
+	while(*p!=' '){//find the first iteration of <space> after the space after the 'write'
 		count+=1;
 		p++;
 	}
-	char add[count-1];
+	char add[count-1];//create a new char
 	strcpy(add,cmd);
-	add[count]='\0';
-	p=&cmd[count+1];
+	add[count]='\0';//change the space to null terminator
+	p=&cmd[count+1];//start after the detected space meaning the string
 	wf=fopen(add,"a+");
 	while(*p!='\0'){
 		fputc(*p,wf);
-		sleep(0.025);
+		sleep(0.025);//sleep every 25 ms
 		p++;
 	}
 	fclose(wf);
